26ee4ac86e9aa12e3446e24dbcbb7982c15f8231
Thiago Souza <thiagossouza07@gmail.com>
2022-02-06 18:24:02 -0300

    Implementando regra de vitória ou derrota no jogo da Forca

--- forca.py
+++ forca.py
@@ -4,24 +4,34 @@ def jogar():
     print("***Bem-vindo ao Jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
     letras_acertadas = ["_","_","_","_","_","_"]
-    print(letras_acertadas)
 
     enforcou = False
     acertou = False
+    erros = 0
+
+    print(letras_acertadas)
 
     while(not enforcou and not acertou):
-        chute = input("Qual a letra? ").strip()
-        index = 0
+        chute = input("Qual a letra? ").strip().upper()
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
 
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index + 1
-
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
 
+    if(acertou):
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
     print("Fim do Jogo!")
 
 if(__name__ == "__main__"):
