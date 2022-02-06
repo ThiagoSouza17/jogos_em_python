be0eca896fdce4ac0c8e1004cc2d25fc173148d4
Thiago Souza <thiagossouza07@gmail.com>
2022-02-06 17:50:42 -0300

    Implementando o Jogo da Forca

--- forca.py
+++ forca.py
@@ -4,7 +4,20 @@ def jogar():
     print("***Bem-vindo ao Jogo da Forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
 
+    while(not enforcou and not acertou):
+        chute = input("Qual a letra? ").strip()
+        index = 0
+
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+
+        print("Jogando...")
     print("Fim do Jogo!")
 
 if(__name__ == "__main__"):
