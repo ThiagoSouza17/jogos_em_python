9880ff72023ac1172495dbbc30c641f81463e962
Thiago Souza <thiagossouza07@gmail.com>
2022-02-06 17:03:45 -0300

    Criando um programa seletor de jogos em Python

---
+++ forca.py
@@ -0,0 +1,13 @@
+
+def jogar():
+    print("*********************************")
+    print("***Bem-vindo ao Jogo da Forca!***")
+    print("*********************************")
+
+
+    print("Fim do Jogo!")
+
+if(__name__ == "__main__"):
+    jogar()
+
+
